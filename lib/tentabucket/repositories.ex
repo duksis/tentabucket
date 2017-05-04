84ba537c7975a84a2c9c63e89381372ca9dfedd3
Jeff Weiss <jweiss@polysync.io>
2017-05-04 15:28:15 -0700

    Update typespec for response
    Prior to this commit the typespec for a repository response was a
    `Tentacat.response`, which is likely a copypasta error. This commit
    updates it to the correct type of response for this library, which is a
    `Tentabucket.response`.

--- lib/tentabucket/repositories.ex
+++ lib/tentabucket/repositories.ex
@@ -10,7 +10,7 @@ defmodule Tentabucket.Repositories do
 
     More info at: https://developer.github.com/v3/repos/#list-your-repositories
   """
-  @spec list_account(binary, Client.t, Keyword.t) :: Tentacat.response
+  @spec list_account(binary, Client.t, Keyword.t) :: Tentabucket.response
   def list_account(owner, client \\ Client.new, opts \\ []) do
     get "repositories/#{owner}", client, opts
   end
